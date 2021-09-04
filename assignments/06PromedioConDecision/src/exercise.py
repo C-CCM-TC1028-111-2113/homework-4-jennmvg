1a9f49b6de6abde6ac6e7666097e72578698cc1c
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-09-03 22:50:44 -0500

    Update exercise.py

--- assignments/06PromedioConDecision/src/exercise.py
+++ assignments/06PromedioConDecision/src/exercise.py
@@ -1,5 +1,21 @@
+import math 
+
 def main():
     #escribe tu código abajo de esta línea
+contar=0
+suma=0
+
+while(True):
+    nota=float(input("Nota: "))
+    
+    if nota<0:
+        break
+    contar= contar+1
+    suma= suma+nota
+
+promedio= suma/contar
+print("Promedio:", promedio)
+
     pass
 if __name__=='__main__':
     main()
