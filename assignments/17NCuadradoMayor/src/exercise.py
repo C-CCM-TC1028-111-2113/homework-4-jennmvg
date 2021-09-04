4ed5b43bb103a40bf3f78794b803b35978be171b
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-09-04 00:16:02 -0500

    Update exercise.py

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,7 +1,16 @@
-
+import math
 
 def main():
     #Escribe tu código debajo de esta línea
+
+n= int(input("ingrese un número:"))
+
+for numero in range (n):
+
+    if numero%2==0:
+        print("#")
+    else: 
+        print ("%")    
     pass
 
 if __name__=='__main__':
