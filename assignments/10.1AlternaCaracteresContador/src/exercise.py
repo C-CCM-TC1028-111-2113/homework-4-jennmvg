2630b75fe30dc73ea7939273c2ed7e92403f1121
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-09-06 11:49:09 -0500

    Update exercise.py

--- assignments/10.1AlternaCaracteresContador/src/exercise.py
+++ assignments/10.1AlternaCaracteresContador/src/exercise.py
@@ -1,5 +1,13 @@
 def main():
     #escribe tu código abajo de esta línea
+    n= int(input("ingrese un número:"))
+
+for numero in range (n):
+
+    if numero%2==0:
+        print("#")
+    else: 
+        print ("%")    
     pass
 
 if __name__=='__main__':   
