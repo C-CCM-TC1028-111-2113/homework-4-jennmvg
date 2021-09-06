ff8e70779d7118b5eaffe8ec53443b19cab4e306
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-09-06 15:58:36 -0500

    Add files via upload

---
+++ assignments/28Fibonacci/src/secuencia fibonacci final.py
@@ -0,0 +1,18 @@
+def main():
+  x=0
+  y=1
+  z=0
+  while True:
+    index=int(input('Enter the index: '))
+    if index>1:
+      break
+    print("0",end = "")
+    pass 
+  for i in range(1,index):
+    z=x+y
+    #print(f"{z}",end=" ")
+    x=y
+    y=z
+  #print("")
+  print (z)
+main()
