2ae13dc3ee2b48c5775e4ef603acc281df381233
jennmvg <88730051+jennmvg@users.noreply.github.com>
2021-09-06 12:23:37 -0500

    Add files via upload

---
+++ assignments/25TrianguloAsteriscos/src/triangulo **.py
@@ -0,0 +1,13 @@
+
+
+
+def main():
+    height = int(input("Enter triangle height: "))
+    #escribe tu código abajo de esta línea
+    for i in range(1, height+1):
+        print (" "*i, "*" * i)
+    
+    pass
+
+if __name__ == '__main__':
+    main()
